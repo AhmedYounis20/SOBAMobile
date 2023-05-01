f0042576136291fafe779332a9822072c5d72bf7
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-05-01 06:23:16 +0300

    adding authentication service

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,70 +1,86 @@
-// import { createContext, useEffect, useState } from "react";
-// import {
-//   loginrequest,
-//   logout,
-//   registerrequest,
-// } from "./authentication.service";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  loginrequest,
+  logout,
+  saveUser,
+  removeUser,
+} from "./authentication.service";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// export const AuthenticationContext = createContext();
-// export const AuthenticationContextProvider = ({ children }) => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [user, setUser] = useState(null);
-//   const [error, setError] = useState("");
 
-//   useEffect(() => console.log(user), []);
-//   onAuthStateChanged(getAuth(), (usr) => {
-//     if (usr) setUser(usr);
-//     setIsLoading(false);
-//   });
+const getSavedUser=async ()=>{
+  const data=await AsyncStorage.getItem("@user");
+  const user=JSON.parse(data);
+  return user;
+}
+export const AuthenticationContext = createContext();
+export const AuthenticationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState("");
+  const [error, setError] = useState("");
 
-//   const onLogin = (email, password) => {
-//     loginrequest(email, password)
-//       .then((user) => {
-//         setUser(user);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         setError(error.toString().split(":").slice(-1));
-//         setIsLoading(false);
-//       });
-//   };
-//   const onLogout = () => {
-//     logout().then(()=>{
-//       setUser(null);
-//       setError(null);
-//     });
-//     console.log("user", user);
-//   };
-//   const onRegister = (email, password, passwordConfirmation) => {
-//     if (password !== passwordConfirmation) {
-//       setError("Error:Password does not match");
-//       return;
-//     }
-//     registerrequest(email, password)
-//       .then((user) => {
-//         setUser(user);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         setError(error.toString().split(":").slice(-1));
-//         setIsLoading(false);
-//       });
-//   };
+  useEffect(() => {
+    const load=async()=>{
+        const usr=await getSavedUser();
+        if (usr) setUser(usr);
+        setIsLoading(false);
+    }
+    load();
+}, []);
 
-//   return (
-//     <AuthenticationContext.Provider
-//       value={{
-//         user,
-//         isLoading,
-//         isAuthenticated: !!user,
-//         error,
-//         onLogin,
-//         onRegister,
-//         onLogout,
-//       }}
-//     >
-//       {children}
-//     </AuthenticationContext.Provider>
-//   );
-// };
+  useEffect(() => {
+    if (user) setUser(user);
+    setIsLoading(false);
+  }, [user]);
+
+  const onLogin = (email, password) => {
+    loginrequest(email, password)
+      .then((user) => {
+        console.log(user);
+        setUser(user);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.toString().split(":").slice(-1));
+        setIsLoading(false);
+      });
+  };
+  const onLogout = () => {
+    logout();
+    removeUser(user);
+    setUser(null);
+    setError(null);
+    console.log("user", user);
+  };
+  const onRegister = (email, password, passwordConfirmation) => {
+    if (password !== passwordConfirmation) {
+      setError("Error:Password does not match");
+      return;
+    }
+    registerrequest(email, password)
+      .then((user) => {
+        setUser(user);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.toString().split(":").slice(-1));
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        user,
+        isLoading,
+        isAuthenticated: !!user,
+        error,
+        onLogin,
+        onRegister,
+        onLogout,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,7 +1,49 @@
-// import { signInWithEmailAndPassword,getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
+export const loginrequest = (email, password) => {
+  const data = {
+    email: email,
+    password: password,
+  };
 
-// export const loginrequest=(email,password)=> signInWithEmailAndPassword(getAuth(),email,password);
+  const load = async () => {
+    console.log("here");
+    try {
+      const user = await fetch(
+        "https://7cf0-156-221-107-245.ngrok-free.app/api/Authentication/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      ).then((res) => res.json());
+      if (user) saveUser(user);
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("here");
+  };
+  load();
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(AsyncStorage.getItem("@user"));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
-// export const registerrequest=(email,password) =>createUserWithEmailAndPassword(getAuth(),email,password);
+export const logout = () => null;
 
-// export const logout=()=> signOut(getAuth());
+export const saveUser = async (user) => {
+  const data = JSON.stringify(user);
+  await AsyncStorage.setItem("@user", `${data}`);
+};
+
+export const removeUser = async (user) => {
+  console.log("user to remove", await AsyncStorage.getItem("@user"));
+  await AsyncStorage.removeItem(`@user`);
+};
