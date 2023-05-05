55978f440943543cbda3779dc07a4507aba95dea
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2023-05-05 16:21:39 +0300

    add sensor card

--- src/appliction/screens/dashboard.screen.js
+++ src/appliction/screens/dashboard.screen.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { React, useState } from "react";
-import { View, Text, Image } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Text } from "../../components/typography/text.component";
+import { Card, Searchbar } from "react-native-paper";
 import { SafeArea } from "../../components/SafeArea/SafeArea.Component";
 import { Spacer } from "../../components/spacer/spacer.component";
 
@@ -21,6 +22,53 @@ const AccountIcon = styled.Image`
   border-radius: 30px;
   margin-top: ${(props) => props.theme.space[3]};
 `;
+
+const MidContainer = styled.View`
+  flex: 1;
+  background-color: blue;
+`;
+
+const SensorCard = styled(Card)`
+  margin: ${(props) => props.theme.space[2]};
+`;
+
+const LocationContainer = styled.View`
+  flex-direction: row;
+  padding: ${(props) => props.theme.space[2]};
+`;
+
+const CurrentTemperature = styled.Text`
+  padding: ${(props) => props.theme.space[2]};
+  font-size: ${(props) => props.theme.fontSizes.h3};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+`;
+
+const CardTop = styled.View``;
+
+const CardBottom = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const SensorReadView = styled.View`
+  flex: 1;
+  margin: ${(props) => props.theme.space[2]};
+`;
+
+const BottomContainer = styled.View`
+  flex: 1;
+  background-color: gray;
+`;
+
+const SensorRead = ({ property = "Humidity", value = "30%" }) => {
+  return (
+    <>
+      <Text variant="body">{property}</Text>
+      <Text variant="label">{value}</Text>
+    </>
+  );
+};
 
 export const DashboardScreen = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -42,9 +90,40 @@ export const DashboardScreen = () => {
           />
         </Spacer>
       </TopContainer>
-      <View>
-        <Text>DashboardScreen</Text>
-      </View>
+      <MidContainer>
+        <SensorCard>
+          <CardTop>
+            <LocationContainer>
+              <Spacer position="right" size="small">
+                <MaterialIcons name="location-pin" size={24} color="black" />
+              </Spacer>
+              <Text variant="body">As Santa</Text>
+              <Spacer position="left" size="xl" />
+              <MaterialCommunityIcons
+                name="weather-pouring"
+                size={24}
+                color="black"
+              />
+            </LocationContainer>
+            <CurrentTemperature>17°C</CurrentTemperature>
+          </CardTop>
+          <CardBottom>
+            <SensorReadView>
+              <SensorRead />
+            </SensorReadView>
+            <SensorReadView>
+              <SensorRead property="Precipitation" value="5.1mL" />
+            </SensorReadView>
+            <SensorReadView>
+              <SensorRead property="Pressure" value="450hPa" />
+            </SensorReadView>
+            <SensorReadView>
+              <SensorRead property="Wind" value="23m/s" />
+            </SensorReadView>
+          </CardBottom>
+        </SensorCard>
+      </MidContainer>
+      <BottomContainer />
     </SafeArea>
   );
 };
