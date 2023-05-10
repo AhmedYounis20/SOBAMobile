892adbc0b601b22b695ba49d342b90d5839c2860
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-05-10 04:23:20 +0300

    adding hairline component

---
+++ src/components/hairlines/hairline.component.js
@@ -0,0 +1,9 @@
+import {View,StyleSheet} from "react-native"
+export const HairLine=()=><View
+style={{
+  borderBottomColor: (props)=>props.theme.colors.text.info,
+  borderBottomWidth: StyleSheet.hairlineWidth,
+  width: "90%",
+  alignSelf: "center",
+}}
+/>;
