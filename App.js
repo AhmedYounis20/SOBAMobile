913c413e09f8051d722d14b8fc448569ddaccd17
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-05-12 11:12:29 +0300

    adding green house screen

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import {
-  AuthenticationContext,
   AuthenticationContextProvider,
 } from "./src/services/authentication/authentication.context";
-import { LoginScreen } from "./src/features/authentication/screens/login.screen";
 import { ThemeProvider } from "styled-components/native";
 import { Navigation } from "./src/infrastructure/navigation";
-import { DashboardScreen } from "./src/features/application/screens/dashboard.screen";
 
 import { theme } from "./src/infrastructure/theme";
 import { LoadFonts } from './src/services/fonts/fonts.services';
@@ -24,9 +19,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- src/infrastructure/navigation/dashboard.navigator.js
+++ src/infrastructure/navigation/dashboard.navigator.js
@@ -1,12 +1,14 @@
 import { TransitionPresets, createStackNavigator } from "@react-navigation/stack";
-import { DashboardScreen } from "../../features/application/screens/dashboard.screen";
+import { DashboardScreen } from "../../features/dashboard/screens/dashboard.screen";
 import { ProfileNavigator } from "./profile.navigator";
+import { GreenHouseScreen } from "../../features/greenhouse/screens/greenhouse.screen";
 
 const DashboardStack=createStackNavigator();
 export const DashboardNavigator=()=>{
 return (
     <DashboardStack.Navigator screenOptions={{headerShown:false,...TransitionPresets.ModalFadeTransition}} initialRouteName="dashboard">
         <DashboardStack.Screen name="dashboard" component={DashboardScreen} />
+        <DashboardStack.Screen name="Greenhouse" component={GreenHouseScreen} />
         <DashboardStack.Screen name="Profile" component={ProfileNavigator} />
     </DashboardStack.Navigator>
 );
