1d97e69438c32a69417ef5e94e3d10696c78547c
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-05-30 13:08:48 +0300

    update sensor control

---
+++ src/features/sensor/components/RoundedButton.component.js
@@ -0,0 +1,31 @@
+import { TouchableOpacity } from "react-native";
+import { Card } from "react-native-paper";
+import { Icon, IconTypes } from "../../../components/Icons/Icons.components";
+
+export const RoundedButton = ({ children }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        height: 60,
+        width: 50,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 20,
+      }}
+    >
+      <Card
+        style={{
+          height: 50,
+          width: 50,
+          borderRadius: 50,
+          backgroundColor: "#1a1a1a",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {children}
+      </Card>
+    </TouchableOpacity>
+  );
+};
--- src/features/sensor/screens/sensorControl.screen.js
+++ src/features/sensor/screens/sensorControl.screen.js
@@ -1,92 +1,166 @@
 import React from "react";
 import { Slider, View } from "react-native";
-// import { Slider } from "@react-native-community/slider";
 import { Text } from "../../../components/typography/text.component";
 import styled from "styled-components";
 import { Card, Switch } from "react-native-paper";
 import CircularProgress from "react-native-circular-progress-indicator";
 import { SafeArea } from "../../../components/SafeArea/SafeArea.Component";
+import { Icon, IconTypes } from "../../../components/Icons/Icons.components";
+import { RoundedButton } from "../components/RoundedButton.component";
 
 const SensorTitle = styled(Text)`
   font-weight: bold;
   font-size: 20px;
 `;
-export const SensorControl = () => {
-  return (
-    <SafeArea>
+
+export const SensorControl = () => (
+  <SafeArea>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 80,
+      }}
+    >
+      <View style={{ marginBottom: 20 }}>
+        <SensorTitle>Sensor Control</SensorTitle>
+      </View>
       <View
         style={{
-          flex: 1,
-          alignItems: "center",
-          paddingTop: 80,
+          flexDirection: "row",
+          width: "95%",
+          justifyContent: "space-between",
+          paddingLeft: 10,
         }}
       >
-        <View style={{ marginBottom: 20 }}>
-          <SensorTitle>Time Setting</SensorTitle>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            maxHeight: 100,
-            width: "90%",
-            justifyContent: "flex-start",
-            paddingLeft: 10,
-          }}
-        >
-          <Switch />
-          <View style={{ width: "65%" }} />
-          <Switch />
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              height: 30,
+            }}
+          >
+            <Switch />
+            <Icon name="fan" iconType={IconTypes.MaterialCommunityIcons} />
+          </View>
+          <Text style={{ fontSize: 10 }}>fan is off when window is open</Text>
         </View>
 
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              height: 30,
+            }}
+          >
+            <Icon name="fan" iconType={IconTypes.MaterialCommunityIcons} />
+            <Switch />
+          </View>
+          <Text style={{ fontSize: 10 }}>fan is off if humidity stable</Text>
+        </View>
+      </View>
+
+      <View
+        style={{
+          height: 400,
+          width: "100%",
+          flexDirection: "row",
+        }}
+      >
         <View
           style={{
-            height: 400,
-            width: "100%",
-            flexDirection: "row",
+            marginTop: 30,
+            marginLeft: 30,
+            justifyContent: "center",
           }}
         >
-          <View style={{ marginTop: 50, marginLeft: 10 }}>
-            <Card
-              style={{
-                height: 280,
-                width: 280,
-                margin: 10,
-                borderRadius: 200,
-                position: "absolute",
-              }}
-            />
-            <CircularProgress
-              value={60}
-              radius={150}
-              duration={1000}
-              maxValue={200}
-              title={"KM/H"}
-              clockwise
-              rotation={180}
-              strokeLinecap="round"
-              inActiveStrokeWidth={5}
-              activeStrokeWidth={8}
-              activeStrokeColor="green"
-              titleStyle={{ fontWeight: "bold" }}
-              titleFontSize={20}
-            />
-          </View>
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Slider
-              style={{
-                width: 200,
-                height: 300,
-                transform: [{ rotate: "-90deg" }],
-              }}
-              minimumValue={0}
-              maximumValue={100}
-            />
-          </View>
+          <Card
+            style={{
+              height: 225,
+              width: 225,
+              alignSelf: "center",
+
+              borderRadius: 200,
+              position: "absolute",
+            }}
+          />
+          <CircularProgress
+            value={60}
+            radius={130}
+            duration={1000}
+            maxValue={200}
+            title={"KM/H"}
+            clockwise
+            rotation={180}
+            strokeLinecap="round"
+            inActiveStrokeWidth={5}
+            activeStrokeWidth={8}
+            activeStrokeColor="green"
+            titleStyle={{ fontWeight: "bold" }}
+            titleFontSize={20}
+          />
         </View>
-        <View></View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            paddingTop: 100,
+          }}
+        >
+          <View style={{ marginBottom: 50 }}>
+            <Text style={{ color: "red", fontWeight: "bold", fontSize: 20 }}>
+              35 ْC
+            </Text>
+          </View>
+          <Slider
+            style={{
+              width: 200,
+              height: 100,
+              transform: [{ rotate: "-90deg" }],
+            }}
+            minimumValue={0}
+            maximumValue={100}
+          />
+        </View>
       </View>
-    </SafeArea>
-  );
-};
+      <View style={{ flexDirection: "row" }}>
+        <RoundedButton>
+          <Icon
+            name={"sun"}
+            iconType={IconTypes.Feather}
+            size={30}
+            color={"white"}
+          />
+        </RoundedButton>
+        <RoundedButton>
+          <Icon
+            name={"water"}
+            iconType={IconTypes.Entypo}
+            size={30}
+            color={"white"}
+          />
+        </RoundedButton>
+        <RoundedButton>
+          <Icon
+            name={"thermometer"}
+            iconType={IconTypes.Feather}
+            size={30}
+            color={"white"}
+          />
+        </RoundedButton>
+        <RoundedButton>
+          <Icon
+            name={"wind"}
+            iconType={IconTypes.Feather}
+            size={30}
+            color={"white"}
+          />
+        </RoundedButton>
+      </View>
+    </View>
+  </SafeArea>
+);
