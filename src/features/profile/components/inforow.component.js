6610cfe21e73cc372f523de45c2558ab5c5f10c4
ahmed ibrahim <99619119+ahmed1567@users.noreply.github.com>
2023-07-03 06:33:48 +0300

    update containers

--- src/features/profile/components/inforow.component.js
+++ src/features/profile/components/inforow.component.js
@@ -7,14 +7,27 @@ import {
   Entypo,
   FontAwesome5,
 } from "@expo/vector-icons";
+import { View } from "react-native";
+import { Text } from "react-native";
 
 const InfoRowView = styled.View`
   flex-direction: row;
   margin: 16px;
+  align-items: center;
 `;
 
 const InfoIconView = styled.View`
   justify-content: center;
+`;
+
+const InfoIconContainer = styled(InfoIconView)`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+  shadow-color: #dbd7d7;
+  background-color: #f5f5f5;
 `;
 
 const InfoDataView = styled.View`
@@ -30,20 +43,34 @@ const ValueText = styled.Text`
   font-family: ${(props) => props.theme.fonts.text};
 `;
 
+const InfoRowContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ArrowContainer = styled.View`
+  margin-right: 5%;
+`;
+
 const getIcon = (property) => {
   switch (property) {
     case "Email":
       return (
-        <MaterialCommunityIcons name="email-outline" size={32} color="gray" />
+        <MaterialCommunityIcons
+          name="email-outline"
+          size={32}
+          color="#27AE60"
+        />
       );
-    case "Mobile":
-      return <Foundation name="telephone" size={32} color="gray" />;
+    case "Telephone":
+      return <Foundation name="telephone" size={32} color="#27AE60" />;
     case "Twitter":
       return <AntDesign name="twitter" size={32} color="gray" />;
-    case "Behance":
-      return <Entypo name="behance" size={32} color="gray" />;
+    case "Location":
+      return <Entypo name="location-pin" size={32} color="#27AE60" />;
     case "Facebook":
-      return <FontAwesome5 name="facebook" size={32} color="gray" />;
+      return <Entypo name="chevron-right" size={32} color="black" />;
     default:
       return <Feather name="info" size={32} color="gray" />;
   }
@@ -51,12 +78,20 @@ const getIcon = (property) => {
 
 export const InfoRow = ({ property, value }) => {
   return (
-    <InfoRowView>
-      <InfoIconView>{getIcon(property)}</InfoIconView>
-      <InfoDataView>
-        <PropertyText>{property}</PropertyText>
-        <ValueText>{value}</ValueText>
-      </InfoDataView>
-    </InfoRowView>
+    <InfoRowContainer>
+      <InfoRowView>
+        <InfoIconContainer elevation={3}>
+          <InfoIconView>{getIcon(property)}</InfoIconView>
+        </InfoIconContainer>
+
+        <InfoDataView>
+          <PropertyText>{property}</PropertyText>
+          <ValueText>{value}</ValueText>
+        </InfoDataView>
+      </InfoRowView>
+      <ArrowContainer>
+        <InfoIconView>{getIcon("Facebook")}</InfoIconView>
+      </ArrowContainer>
+    </InfoRowContainer>
   );
 };
