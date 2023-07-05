129d05c9aa8c823a61a8943a5290d723239177b7
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-07-05 14:27:22 +0300

    add notification navigation

---
+++ src/features/notifications/screens/notifications.screen.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { View } from "react-native";
+
+export const NotificationScreen = () => {
+  return (
+    <View>
+      <Text>Notifications</Text>
+    </View>
+  );
+};
