880fedb402a6016c4d26904797c225acba5ce64a
ahmed ibrahim <99619119+ahmed1567@users.noreply.github.com>
2023-07-13 00:25:01 +0300

    add line chart and seed data

--- src/features/assistant/screens/assistant.screen.js
+++ src/features/assistant/screens/assistant.screen.js
@@ -1,18 +1,12 @@
 import { NotificationScreen } from "../../notifications/screens/notifications.screen";
 import React, { useContext, useEffect, useState } from "react";
-import { View } from "react-native";
-import Slider from "@react-native-community/slider";
-import { Text } from "../../../components/typography/text.component";
+import { TextInput, View, Text, ScrollView } from "react-native";
 import styled from "styled-components";
-import { Switch } from "react-native-paper";
-import CircularProgress from "react-native-circular-progress-indicator";
 import { SafeArea } from "../../../components/SafeArea/SafeArea.Component";
-import { Icon, IconTypes } from "../../../components/Icons/Icons.components";
 
 import { LineChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 import { ThemeContext } from "../../../services/ThemeContext/Theme.context";
-import { ScreenView } from "../../../components/views/screenView.component";
 import {
   Chart,
   VerticalAxis,
@@ -21,9 +15,13 @@ import {
 } from "react-native-responsive-linechart";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
+export const ItemsCenter = styled.View`
+  align-items: center;
+`;
+
 export const Background = styled.View`
   height: ${Platform.OS == "android" ? "12%" : "14%"};
-  width: 100%;
+  width: 100 %;
   top: 0px;
   right: 0px;
   background-color: ${(props) => props.theme.colors.ui.primary};
@@ -31,128 +29,267 @@ export const Background = styled.View`
   border-bottom-left-radius: 12px;
   justify-content: center;
 `;
-export const SupportTitle = styled.Text`
+export const HarvestTitle = styled.Text`
 font-family : ${(props) => props.theme.fonts.title}
 font-size: ${(props) => props.theme.fontSizes.title};
 color: ${(props) => props.theme.colors.text.white};
-margin-left:8%
+margin-left:8 %
 `;
+
+export const FormBox = styled.View`
+  position: absolute;
+  width: 95%;
+  background-color: ${(props) => props.theme.colors.ui.primary};
+  z-index: 5;
+  border-radius: 25px;
+  align-self: center;
+
+  top: 30 %;
+`;
+
+export const FieldContainer = styled.View`
+  padding-vertical: 2 %;
+`;
+
+export const FieldTextContainer = styled.View`
+  align-items: center;
+  margin-left: 5 %;
+  padding-bottom: 4 %;
+  flex-direction: row;
+`;
+export const InputLabel = styled.Text`
+  font-size: 17px;
+  font-weight: 600;
+  margin-right: 1 %;
+  color: ${(props) => props.theme.colors.text.disabled};
+`;
+
+export const InputFieldContainer = styled.View`
+  width: 90 %;
+  background-color: ${(props) => props.theme.colors.bg.inverse2};
+  border-radius: 10px;
+  align-self: center;
+`;
+
+export const SaveContainer = styled.TouchableOpacity`
+  padding: 2.5%;
+  background-color: ${(props) => props.theme.colors.ui.primary};
+  width: 90%;
+  align-self: center;
+  border-radius: 25px;
+  align-items: center;
+`;
+export const AddContainer = styled(SaveContainer)`
+  width: 95%;
+`;
+
+export const CropText = styled.Text`
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  color: ${(props) => props.theme.colors.text.primary};
+`;
+
 export const AssistantScreen = () => {
   const { theme } = useContext(ThemeContext);
+  const [harvest, setHarvest] = useState({});
+  const [cropTypes, setCropTypes] = useState([
+    "apple",
+    "orange",
+    "bannana",
+    "tomatoes",
+  ]);
   const [isAdd, setIsAdd] = useState(false);
+  const [month, setMonth] = useState("");
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+
   const OnAdd = () => {
-    setIsAdd(true);
+    setIsAdd(!isAdd);
+  };
+  const OnSave = () => {
+    if (name && harvest[name]) {
+      amountNumber = Number(amount);
+      harvest[name]["amounts"].push(amountNumber);
+      if (!harvest[name]["months"].includes(month)) {
+        harvest[name]["months"].push(month);
+      }
+    } else if (name) {
+      harvest[name] = {};
+      harvest[name]["months"] = [];
+      harvest[name]["amounts"] = [];
+      amountNumber = Number(amount);
+      harvest[name]["amounts"].push(amountNumber);
+      if (!harvest[name]["months"].includes(month)) {
+        harvest[name]["months"].push(month);
+      }
+    }
+
+    console.log(harvest);
+    const types = Object.keys(harvest);
+    setCropTypes(types);
+    setCropTypes(types);
+
+    setIsAdd(!isAdd);
   };
 
-  const TimeSeriesData = [
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-    Math.random() * 100,
-  ];
+  useEffect(() => {
+    console.log(cropTypes);
+    setHarvest({
+      apple: {
+        amount: [500, 600, 550, 650],
+        month: ["mar", "apr", "aug", "oct"],
+      },
+      orange: {
+        amount: [500, 600, 550, 650, 700],
+        month: ["mar", "apr", "aug", "oct", "nov"],
+      },
+      bannana: {
+        amount: [500, 600, 700],
+        month: ["mar", , "oct", "nov"],
+      },
+      tomatoes: {
+        amount: [100, 200, 250, 350, 300],
+        month: ["mar", "apr", "aug", "oct", "nov"],
+      },
+    });
+    console.log(harvest);
+  }, []);
+
   return (
     <SafeArea>
       <Background>
-        <SupportTitle>Harvest</SupportTitle>
+        <HarvestTitle>Harvest</HarvestTitle>
       </Background>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {cropTypes &&
+          cropTypes.map((crop, index) => (
+            <View key={index}>
+              <View style={{ marginVertical: "1.5%", alignItems: "center" }}>
+                <CropText>{crop}</CropText>
+              </View>
+              <View style={{ alignSelf: "center" }}>
+                <LineChart
+                  data={{
+                    labels: harvest[crop]["month"],
+                    datasets: [
+                      {
+                        data: harvest[crop]["amount"],
+                      },
+                    ],
+                  }}
+                  width={Dimensions.get("window").width - 10} // from react-native
+                  height={200}
+                  yAxisLabel=""
+                  yAxisSuffix="ْkg"
+                  yAxisInterval={1} // optional, defaults to 1
+                  chartConfig={{
+                    backgroundGradientFrom: theme.colors.bg.secondary,
+                    backgroundGradientTo: theme.colors.bg.secondary,
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(120, 120, 120, ${opacity})`,
+                    labelColor: (opacity = 1) =>
+                      `rgba(120, 120, 120, ${opacity})`,
+                    style: {
+                      borderRadius: 16,
+                    },
+                    propsForDots: {
+                      r: "6",
+                      strokeWidth: "2",
+                      stroke: "#ffa726",
+                    },
+                    propsForLabels: {
+                      transform: [{ rotate: "-45deg" }], // Rotate the labels by -45 degrees
+                    },
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                  }}
+                />
+              </View>
+            </View>
+          ))}
+      </ScrollView>
       <View
         style={{
-          position: "absolute",
-          top: "35%",
+          marginBottom: "1%",
           alignSelf: "center",
+          width: "100%",
         }}
       >
-        <LineChart
-          data={{
-            labels: [
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ],
-            datasets: [
-              {
-                data: TimeSeriesData,
-              },
-            ],
-          }}
-          width={Dimensions.get("window").width - 10} // from react-native
-          height={280}
-          yAxisLabel=""
-          yAxisSuffix="ْkg"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundGradientFrom: theme.colors.bg.secondary,
-            backgroundGradientTo: theme.colors.bg.secondary,
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(120, 120, 120, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(120, 120, 120, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "#ffa726",
-            },
-            propsForLabels: {
-              transform: [{ rotate: "-45deg" }], // Rotate the labels by -45 degrees
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
-        <TouchableOpacity
-          style={{
-            alignSelf: "center",
-            alignItems: "center",
-            marginTop: "10%",
-            width: "95%",
-            backgroundColor: "red",
-            borderRadius: 25,
-          }}
-          onPress={OnAdd}
-        >
-          <Text style={{ fontSize: 18, fontWeight: "600" }}>Add</Text>
-        </TouchableOpacity>
+        {!isAdd && (
+          <AddContainer onPress={OnAdd}>
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>Add</Text>
+          </AddContainer>
+        )}
       </View>
       {isAdd && (
-        <View
-          style={{
-            position: "absolute",
-            width: "95%",
-            borderRadius: 25,
-            backgroundColor: "red",
-            top: "50%",
-            alignSelf: "center",
-          }}
-        >
-          <View style={{ alignItems: "center" }}>
-            <Text>ddffdfd</Text>
-          </View>
-        </View>
+        <FormBox>
+          <FieldContainer>
+            <FieldTextContainer>
+              <InputLabel> Harvest Month</InputLabel>
+            </FieldTextContainer>
+
+            <InputFieldContainer>
+              <TextInput
+                onChangeText={(input) => setMonth(input)}
+                placeholder="Month"
+                style={{
+                  height: 20,
+                  margin: 12,
+                  borderWidth: 1,
+                  borderColor: "transparent",
+                  color: "black",
+                }}
+              />
+            </InputFieldContainer>
+          </FieldContainer>
+          <FieldContainer>
+            <FieldTextContainer>
+              <InputLabel> Crop Name</InputLabel>
+            </FieldTextContainer>
+
+            <InputFieldContainer>
+              <TextInput
+                onChangeText={(input) => setName(input)}
+                placeholder="crop name"
+                style={{
+                  height: 20,
+                  margin: 12,
+                  borderWidth: 1,
+                  borderColor: "transparent",
+                  color: "black",
+                }}
+              />
+            </InputFieldContainer>
+          </FieldContainer>
+          <FieldContainer>
+            <FieldTextContainer>
+              <InputLabel> Amount</InputLabel>
+            </FieldTextContainer>
+
+            <InputFieldContainer>
+              <TextInput
+                onChangeText={(input) => setAmount(input)}
+                placeholder="Amount"
+                style={{
+                  height: 20,
+                  margin: 12,
+                  borderWidth: 1,
+                  borderColor: "transparent",
+                  color: "black",
+                }}
+              />
+            </InputFieldContainer>
+          </FieldContainer>
+          <FieldContainer>
+            <SaveContainer onPress={OnSave}>
+              <Text style={{ fontSize: 18, fontWeight: "600" }}>Save</Text>
+            </SaveContainer>
+          </FieldContainer>
+        </FormBox>
       )}
     </SafeArea>
   );
